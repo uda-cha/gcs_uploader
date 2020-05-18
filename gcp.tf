fd4a09a38541b8032ac463f6c77a6c6eb6970572
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-05-18 23:03:17 +0900

    mv bucket name to tfvars

--- gcp.tf
+++ gcp.tf
@@ -1,6 +1,7 @@
 variable "credentials" {}
 variable "project" {}
 variable "region" {}
+variable "bucket" {}
 
 provider "google" {
   credentials  = file(var.credentials)
@@ -17,7 +18,7 @@ terraform {
 }
 
 resource "google_storage_bucket" "backup" {
-  name          = "samba-backup"
+  name          = var.bucket
   location      = var.region
   storage_class = "STANDARD"
   bucket_policy_only = true
--- sample.terraform.tfvars
+++ sample.terraform.tfvars
@@ -1,3 +1,4 @@
 credentials  = "./.credentials/account.json"
 project      = "your_gcp_project_id"
 region       = "your_gcs_region"
+bucket       = "your_backet_name"
