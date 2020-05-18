953fe5a0b96316eaa8473f1cab86ea1526be88fe
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-05-18 22:54:59 +0900

    add upload.sh

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 $ docker-compose run --rm terraform plan -out tfplan
 $ docker-compose run --rm terraform apply "tfplan"
 ```
+
+## GCSへのアップロード実行
+
+以下の環境変数を設定する
+
+* `SRC_PATH`
+* `BUCKET_NAME`
+
+```sh
+$ docker-compose run --rm gcloud bin/upload.sh
+```
--- bin/upload.sh
+++ bin/upload.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -eu
 
 gcloud auth activate-service-account --key-file /workdir/.credentials/account.json
+
+echo [`date +"%Y/%m/%d %H:%M:%S"`] excuting gsutil -m rsync -r ${SRC_PATH} gs://${BUCKET_NAME}...
+gsutil -m rsync -r ${SRC_PATH} gs://${BUCKET_NAME}
+echo [`date +"%Y/%m/%d %H:%M:%S"`] done.
